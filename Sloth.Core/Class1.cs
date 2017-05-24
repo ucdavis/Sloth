90d8a8185aa7fbee6b4b971eae401404af6c7231
John Knoll <jpknoll@gmail.com>
2017-05-24 16:55:08 -0700

    new projects :-D

---
+++ Sloth.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sloth.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sloth.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sloth.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sloth.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee6062c5-b911-4cbc-9c81-e05f20dc5108")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
