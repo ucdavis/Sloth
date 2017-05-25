1d9bf871009c53f45bcbee6d46e6a6f4e8dc0e3c
John Knoll <jpknoll@gmail.com>
2017-05-25 16:39:56 -0700

    use swashbuckle

--- Sloth.Api/Startup.cs
+++ Sloth.Api/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -8,8 +9,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
 using Sloth.Api.Data;
 using Sloth.Core;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Sloth.Api
 {
@@ -41,6 +44,30 @@ namespace Sloth.Api
 
             // Add framework services.
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Title = "Sloth API v1",
+                    Version = "v1",
+                    Description = "Scrubber Loader & Online Transaction Hub",
+                    Contact = new Contact()
+                    {
+                        Name = "John Knoll",
+                        Email = "jpknoll@ucdavis.edu"
+                    },
+                    License = new License()
+                    {
+                        Name = "MIT",
+                        Url = "https://www.github.com/ucdavis/sloth/LICENSE"
+                    },
+                    Extensions =
+                    {
+                        { "ProjectUrl", "https://www.github.com/ucdavis/sloth" }
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +77,14 @@ namespace Sloth.Api
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            app.UseSwagger(o =>
+            {
+            });
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sloth API v1");
+            });
 
             DbInitializer.Initialize(context);
         }
