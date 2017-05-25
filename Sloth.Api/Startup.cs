538624ff4248db9c467a4a615a93f9c56fcd42e6
John Knoll <jpknoll@gmail.com>
2017-05-25 12:04:23 -0700

    add context

--- Sloth.Api/Startup.cs
+++ Sloth.Api/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Sloth.Core;
 
 namespace Sloth.Api
 {
@@ -29,6 +30,8 @@ namespace Sloth.Api
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<SlothDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
