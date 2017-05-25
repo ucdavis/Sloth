48522f4e8f8c8e212ab00af2080875645c9b2bc4
John Knoll <jpknoll@gmail.com>
2017-05-25 16:39:06 -0700

    add version to route

---
+++ Sloth.Api/Resources/VersionString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sloth.Api.Resources
+{
+    public static class VersionString
+    {
+        public const string Current = Version1;
+
+        public const string Version1 = "1";
+    }
+}
