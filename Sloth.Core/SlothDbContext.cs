9f20551d5795c1ee51c84932655983bd786842ff
John Knoll <jpknoll@gmail.com>
2017-05-25 12:04:09 -0700

    dbcontext!

---
+++ Sloth.Core/SlothDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Sloth.Core.Models;
+
+namespace Sloth.Core
+{
+    public partial class SlothDbContext : DbContext
+    {
+        public DbSet<Scrubber> Scrubbers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\sqlexpress;Database=sloth;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
