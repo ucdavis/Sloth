d723c4820fa0d21cb1a4b2d601ad1a54fddc22ea
John Knoll <jpknoll@gmail.com>
2017-06-05 15:49:00 -0700

    base class to simplify namespacing

---
+++ Sloth.Xml/KfsXmlElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Sloth.Xml
+{
+    public abstract class KfsXmlElement
+    {
+        protected KfsXmlElement()
+        {
+            Namespaces = new XmlSerializerNamespaces(new[]
+            {
+                new XmlQualifiedName(string.Empty, KfsNamespace), 
+            });
+        }
+
+        public const string KfsNamespace = "http://www.kuali.org/kfs/gl/collector";
+
+        [XmlNamespaceDeclarations]
+        public XmlSerializerNamespaces Namespaces { get; }
+    }
+}
