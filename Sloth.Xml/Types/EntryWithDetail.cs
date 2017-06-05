871e2c0969432dcb303cd287986bc105a6a23c53
John Knoll <jpknoll@gmail.com>
2017-06-05 15:49:15 -0700

    group details with entry

---
+++ Sloth.Xml/Types/EntryWithDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Sloth.Xml.Types
+{
+    [XmlRoot("entry", Namespace = KfsNamespace, IsNullable = false)]
+    public class EntryWithDetail : Entry
+    {
+        [XmlIgnore]
+        public Detail Detail { get; set; }
+    }
+}
