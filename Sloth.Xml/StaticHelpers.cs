bd52f28e299585a1555dd026a17aacaddc01a9a6
John Knoll <jpknoll@gmail.com>
2017-06-05 15:49:08 -0700

    make standard serializer static

---
+++ Sloth.Xml/StaticHelpers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Sloth.Xml
+{
+    public static class StaticHelpers<T>
+    {
+        public static readonly XmlSerializer XmlSerializer = new XmlSerializer(typeof(T));
+    }
+}
