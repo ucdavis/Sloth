37a14ce33cb933e316c294c83c8ca2cab83c84ae
John Knoll <jpknoll@gmail.com>
2017-06-06 12:14:07 -0700

    handler for requirement

---
+++ Sloth.Api/Identity/ApiKeyHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Sloth.Api.Identity
+{
+    public class ApiKeyHandler : AuthorizationHandler<ApiKeyRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ApiKeyRequirement requirement)
+        {
+            if (context.User.HasClaim(c => c.Type == ClaimTypes.Name))
+            {
+                return Task.CompletedTask;
+            }
+
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+    }
+}
