92736c1195a07db7f68ec100b44293b56a051553
John Knoll <jpknoll@gmail.com>
2017-06-06 12:13:07 -0700

    new model

---
+++ Sloth.Core/Models/ApiKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sloth.Core.Models
+{
+    public class ApiKey
+    {
+        public string Id { get; set; }
+        public string Owner { get; set; }
+        public DateTime Issued { get; set; }
+        public DateTime? Revoked { get; set; }
+    }
+}
