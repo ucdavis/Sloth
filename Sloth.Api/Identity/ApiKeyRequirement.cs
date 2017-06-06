39c83e5bdf50d19fc518cbfba2b9e895950bfba2
John Knoll <jpknoll@gmail.com>
2017-06-06 12:14:01 -0700

    new requirement

---
+++ Sloth.Api/Identity/ApiKeyRequirement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Sloth.Api.Identity
+{
+    public class ApiKeyRequirement : IAuthorizationRequirement
+    {
+        public ApiKeyRequirement()
+        {
+        }
+    }
+}
