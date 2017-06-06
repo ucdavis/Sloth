4853d71e3558ac8a17547b837661c096f911d77f
John Knoll <jpknoll@gmail.com>
2017-06-06 12:13:52 -0700

    run delete, migrate, create

--- Sloth.Api/Data/DbInitializer.cs
+++ Sloth.Api/Data/DbInitializer.cs
@@ -1,27 +1,54 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Sloth.Core;
 using Sloth.Core.Models;
 
 namespace Sloth.Api.Data
 {
+    /// <summary>
+    /// Creates sample data for development environments
+    /// </summary>
     public static class DbInitializer
     {
+        /// <summary>
+        /// Create sample data
+        /// </summary>
+        /// <param name="context"></param>
         public static void Initialize(SlothDbContext context)
         {
-            context.Database.EnsureCreated();
+            context.Database.EnsureDeleted();
+            context.Database.Migrate();
 
-            if (context.Scrubbers.Any())
+            if (!context.ApiKeys.Any())
             {
-                return;
+                CreateApikeys(context);
             }
 
-            var scrubbers = new Scrubber[]
+            if (!context.Scrubbers.Any())
+            {
+                CreateScrubbers(context);
+            }
+
+            context.SaveChanges();
+        }
+
+        private static void CreateApikeys(SlothDbContext context)
+        {
+            var apiKeys = new[]
+            {
+                new ApiKey() {Id = "TestKey123", Owner = "John", Issued = DateTime.UtcNow},
+            };
+            context.ApiKeys.AddRange(apiKeys);
+        }
+
+        private static void CreateScrubbers(SlothDbContext context)
+        {
+            var scrubbers = new[]
             {
                 new Scrubber(),
             };
             context.Scrubbers.AddRange(scrubbers);
-            context.SaveChanges();
         }
     }
 }
