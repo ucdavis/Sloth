5a6f988f3225cc24acddf053d5cbe07592b42776
John Knoll <jpknoll@gmail.com>
2017-06-22 15:37:02 -0700

    new hangfire job server

---
+++ Sloth.Jobs.HangfireServer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using Hangfire;
+using Microsoft.Azure.WebJobs;
+
+namespace Sloth.Jobs.HangfireServer
+{
+    public class Program
+    {
+        private static BackgroundJobServerOptions _options;
+        private static CancellationToken _cancellationToken;
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                // configure handfire job processor
+                GlobalConfiguration.Configuration.UseSerilogLogProvider();
+
+                // configure job server
+                _options = new BackgroundJobServerOptions()
+                {
+
+                };
+
+                // listen for shutdon
+                _cancellationToken = new WebJobsShutdownWatcher().Token;
+
+                // startup job server
+                using (var server = new BackgroundJobServer(_options))
+                {
+                    while (true)
+                    {
+                        if (_cancellationToken.IsCancellationRequested)
+                        {
+                            server.SendStop();
+                            break;
+                        }
+
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
