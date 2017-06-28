01bd30da1a82e483b3a8f5dd76e7b18c4a34fd2b
John Knoll <jpknoll@gmail.com>
2017-06-28 11:51:11 -0700

    ilist helper

---
+++ Sloth.Api/Helpers/ListHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sloth.Api.Helpers
+{
+    public static class ListHelpers
+    {
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            // ReSharper disable once CanBeReplacedWithTryCastAndCheckForNull
+            if (list is List<T>)
+            {
+                ((List<T>)list).AddRange(items);
+                return;
+            }
+
+            foreach (var item in items)
+            {
+                list.Add(item);
+            }
+        }
+
+    }
+}
