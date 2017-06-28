f9b82642829babd01097ceddd31237cbf0785a0a
John Knoll <jpknoll@gmail.com>
2017-06-28 11:51:29 -0700

    yet another cron string

--- Sloth.Api/Jobs/CronStrings.cs
+++ Sloth.Api/Jobs/CronStrings.cs
@@ -2,6 +2,19 @@
 {
     internal class CronStrings
     {
+        /// <summary>
+        /// Every Minute
+        /// </summary>
         public const string Minutely = "* * * * *";
+
+        /// <summary>
+        /// Each Night
+        /// </summary>
+        public const string Nightly = "0 23 * * *";
+
+        /// <summary>
+        /// 4pm weekdays
+        /// </summary>
+        public const string EndOfBusiness = "0 14 * * 1,2,3,4,5";
     }
 }
