20c4d5b3e1b578adb96d446482d79f627d0032f8
John Knoll <jpknoll@gmail.com>
2017-06-28 11:53:19 -0700

    add service provider method

--- Sloth.Jobs.HangfireServer/Program.cs
+++ Sloth.Jobs.HangfireServer/Program.cs
@@ -3,11 +3,14 @@ using System.Threading;
 using Hangfire;
 using Hangfire.Console;
 using Microsoft.Azure.WebJobs;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Sloth.Api;
 using Sloth.Api.Jobs;
+using Sloth.Api.Services;
+using Sloth.Core;
 using Sloth.Jobs.HangfireServer.Logging;
 
 namespace Sloth.Jobs.HangfireServer
@@ -32,10 +35,7 @@ namespace Sloth.Jobs.HangfireServer
                 LoggingConfiguration.Setup(Configuration);
 
                 // configure DI
-                var serviceProvider = new ServiceCollection()
-                    .AddTransient(s => LoggingConfiguration.Configuration)
-                    .AddTransient<Heartbeat>()
-                    .BuildServiceProvider();
+                var serviceProvider = BuildServiceProvider();
 
                 // configure handfire job processor
                 GlobalConfiguration.Configuration
@@ -75,6 +75,32 @@ namespace Sloth.Jobs.HangfireServer
                 Log.Error(ex, ex.Message);
                 throw;
             }
+        }
+
+        private static IServiceProvider BuildServiceProvider()
+        {
+            var services = new ServiceCollection();
+
+            // expose configuration
+            services.AddSingleton<IConfiguration>(_ => Configuration);
+
+            // add database
+            services.AddDbContext<SlothDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            // add infrastructure services
+            services.AddTransient<IStorageService, StorageService>();
+
+            // add logging setup
+            services.AddTransient(s => LoggingConfiguration.Configuration);
+
+            // add jobs
+            services.AddTransient<Heartbeat>();
+            services.AddTransient<UploadScrubberJob>();
+
+            return services.BuildServiceProvider();
         }
 
         private static IConfigurationRoot Configuration { get; set; }
