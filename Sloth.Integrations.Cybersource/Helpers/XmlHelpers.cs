513d1b956f73c074f69a8b75dff06f833ecb9984
John Knoll <jpknoll@gmail.com>
2017-07-24 15:51:30 -0700

    namespace regex

---
+++ Sloth.Integrations.Cybersource/Helpers/XmlHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Sloth.Integrations.Cybersource.Helpers
+{
+    public static class XmlHelpers
+    {
+        public static string RemoveAllNamespaces(string xml)
+        {
+            const string pattern = "\\s+xmlns\\s*(:\\w)?\\s*=\\s*\\\"(?<url>[^\\\"]*)\\\"";
+            return Regex.Replace(xml, pattern, "");
+        }
+    }
+}
