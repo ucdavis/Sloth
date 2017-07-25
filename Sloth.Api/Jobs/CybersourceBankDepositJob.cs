77cd1a6fc0cf185de9dbcf62cf9a94840ea84919
John Knoll <jpknoll@gmail.com>
2017-07-25 14:57:19 -0700

    try/catch per integration

--- Sloth.Api/Jobs/CybersourceBankDepositJob.cs
+++ Sloth.Api/Jobs/CybersourceBankDepositJob.cs
@@ -36,6 +36,7 @@ namespace Sloth.Api.Jobs
             var log = Logger.ForContext("jobId", context.BackgroundJob.Id);
 
             var yesterday = DateTime.UtcNow.Date.AddDays(-1);
+            log = log.ForContext("date", yesterday);
 
             try
             {
@@ -43,74 +44,84 @@ namespace Sloth.Api.Jobs
 
                 foreach (var integration in integrations)
                 {
-                    // fetch password secret
-                    var password = await _secretsService.GetSecret(integration.ReportPasswordKey);
-
-                    // create client
-                    var client = new ReportClient(_cybersourceSettings.ReportUrl, integration.MerchantId, integration.ReportUsername, password);
-
-                    // fetch report
-                    var report = await client.GetPaymentBatchDetailReport(yesterday);
-                    var count = report.Batches?.Batch?.Length ?? 0;
-
-                    log.Information("Report found with {count} records.", new { count });
-                    if (count < 1)
+                    try
                     {
-                        continue;
-                    }
+                        // fetch password secret
+                        var password = await _secretsService.GetSecret(integration.ReportPasswordKey);
 
-                    // iterate over deposits
-                    foreach (var deposit in report.Batches?.Batch?.SelectMany(b => b.Requests?.Request) ?? new List<Request>())
+                        // create client
+                        var client = new ReportClient(_cybersourceSettings.ReportUrl, integration.MerchantId, integration.ReportUsername, password);
+
+                        // fetch report
+                        var report = await client.GetPaymentBatchDetailReport(yesterday);
+                        var count = report.Batches?.Batch?.Length ?? 0;
+
+                        log.ForContext("integration", integration.Id)
+                           .Information("Report found with {count} records.", new { count });
+
+                        if (count < 1)
+                        {
+                            continue;
+                        }
+
+                        // iterate over deposits
+                        foreach (var deposit in report.Batches?.Batch?.SelectMany(b => b.Requests?.Request) ?? new List<Request>())
+                        {
+                            // create transaction per deposit item,
+                            // moving monies from clearing to holding account
+                            var transaction = new Transaction()
+                            {
+                                Status = TransactionStatus.Scheduled
+                            };
+
+                            var clearing = new Transfer()
+                            {
+                                Account = _cybersourceSettings.ClearingAccount,
+                                Direction = Transfer.CreditDebit.Debit,
+                                Amount = deposit.Amount,
+                                TrackingNumber = deposit.MerchantReferenceNumber,
+                            };
+                            transaction.Transfers.Add(clearing);
+
+                            var holding = new Transfer()
+                            {
+                                Account = _cybersourceSettings.HoldingAccount,
+                                Direction = Transfer.CreditDebit.Credit,
+                                Amount = deposit.Amount,
+                                TrackingNumber = deposit.MerchantReferenceNumber
+                            };
+                            transaction.Transfers.Add(holding);
+
+                            // then from holding to the default account
+                            var holding2 = new Transfer()
+                            {
+                                Account = _cybersourceSettings.HoldingAccount,
+                                Direction = Transfer.CreditDebit.Debit,
+                                Amount = deposit.Amount,
+                                TrackingNumber = deposit.MerchantReferenceNumber
+                            };
+                            transaction.Transfers.Add(holding2);
+
+                            var final = new Transfer()
+                            {
+                                Account = integration.DefaultAccount,
+                                Direction = Transfer.CreditDebit.Credit,
+                                Amount = deposit.Amount,
+                                TrackingNumber = deposit.MerchantReferenceNumber
+                            };
+                            transaction.Transfers.Add(final);
+
+                            _context.Transactions.Add(transaction);
+                        }
+
+                        // push changes for this integration
+                        _context.SaveChanges();
+                    }
+                    catch (Exception ex)
                     {
-                        // create transaction per deposit item,
-                        // moving monies from clearing to holding account
-                        var transaction = new Transaction()
-                        {
-                            Status = TransactionStatus.Scheduled
-                        };
-
-                        var clearing = new Transfer()
-                        {
-                            Account        = _cybersourceSettings.ClearingAccount,
-                            Direction      = Transfer.CreditDebit.Debit,
-                            Amount         = deposit.Amount,
-                            TrackingNumber = deposit.MerchantReferenceNumber,
-                        };
-                        transaction.Transfers.Add(clearing);
-
-                        var holding = new Transfer()
-                        {
-                            Account        = _cybersourceSettings.HoldingAccount,
-                            Direction      = Transfer.CreditDebit.Credit,
-                            Amount         = deposit.Amount,
-                            TrackingNumber = deposit.MerchantReferenceNumber
-                        };
-                        transaction.Transfers.Add(holding);
-
-                        // then from holding to the default account
-                        var holding2 = new Transfer()
-                        {
-                            Account        = _cybersourceSettings.HoldingAccount,
-                            Direction      = Transfer.CreditDebit.Debit,
-                            Amount         = deposit.Amount,
-                            TrackingNumber = deposit.MerchantReferenceNumber
-                        };
-                        transaction.Transfers.Add(holding2);
-
-                        var final = new Transfer()
-                        {
-                            Account        = integration.DefaultAccount,
-                            Direction      = Transfer.CreditDebit.Credit,
-                            Amount         = deposit.Amount,
-                            TrackingNumber = deposit.MerchantReferenceNumber
-                        };
-                        transaction.Transfers.Add(final);
-
-                        _context.Transactions.Add(transaction);
+                        log.ForContext("integration", integration.Id)
+                           .Error(ex, ex.Message);
                     }
-
-                    // push changes for this integration
-                    _context.SaveChanges();
                 }
             }
             catch (Exception ex)
