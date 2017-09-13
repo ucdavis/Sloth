97652e33e4b41bf49dbe9d75e33ffe6f401e40f3
John Knoll <jpknoll@gmail.com>
2017-09-13 11:00:21 -0700

    use new table

--- Sloth.Web/ClientApp/routes.tsx
+++ Sloth.Web/ClientApp/routes.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Transactions } from './components/Transactions';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
+    <Route path='/transactions' component={ Transactions } />
 </Layout>;
