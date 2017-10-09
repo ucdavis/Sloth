8786aea2ec188575c81dc572c05b3a6f0483dd14
John Knoll <jpknoll@gmail.com>
2017-10-09 13:54:16 -0700

    unused methods
    remove authentication requirement from hom

--- Sloth.Web/Controllers/HomeController.cs
+++ Sloth.Web/Controllers/HomeController.cs
@@ -1,28 +1,13 @@
 using System.Diagnostics;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Sloth.Web.Models;
 
 namespace Sloth.Web.Controllers
 {
-    public class HomeController : SuperController
+    public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
