aa156b924839b8bee18b336a8daa82694c2a3cb7
John Knoll <jpknoll@gmail.com>
2017-10-10 15:27:30 -0700

    Add email hash

--- Sloth.Core/Models/User.cs
+++ Sloth.Core/Models/User.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
@@ -19,6 +21,33 @@ namespace Sloth.Core.Models
         public string UserName { get; set; }
 
         public string Email { get; set; }
+
+        [JsonIgnore]
+        public string EmailHash
+        {
+            get
+            {
+                // Create a new instance of the MD5CryptoServiceProvider object.
+                var md5Hasher = MD5.Create();
+
+                // Convert the input string to a byte array and compute the hash.
+                var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(Email.ToLower().Trim()));
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                var sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data
+                // and format each one as a hexadecimal string.
+                foreach (var t in data)
+                {
+                    sBuilder.Append(t.ToString("x2"));
+                }
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString(); 
+            }
+        }
 
         [JsonIgnore]
         public IList<UserRole> UserRoles { get; set; }
