75727abddd0b36f6695f7cbc290b6c30d0c2d0ce
John Knoll <jpknoll@gmail.com>
2018-01-03 15:57:29 -0800

    add various validations

--- Sloth.Core/Models/Source.cs
+++ Sloth.Core/Models/Source.cs
@@ -8,18 +8,31 @@ namespace Sloth.Core.Models
     {
         public string Id { get; set; }
 
+        [MaxLength(50)]
         [Required]
         public string Name { get; set; }
 
+        [MaxLength(50)]
         [Required]
         public string Type { get; set; }
 
+        [MaxLength(255)]
         public string Description { get; set; }
 
+        /// <summary>
+        /// Unique feed origination identifier given to the Feed System.
+        /// The origination code is validated in during file receipt and in the processing.
+        /// </summary>
+        [MinLength(2)]
         [MaxLength(2)]
         [Required]
         public string OriginCode { get; set; }
 
+        /// <summary>
+        /// Financial System document type associated with the feed.
+        /// Feed systems will be authorized to use a specific value based on transactions.
+        /// </summary>
+        [MinLength(4)]
         [MaxLength(4)]
         [Required]
         public string DocumentType { get; set; }
