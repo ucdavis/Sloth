1aeb3eb18c6806eae91bef1078037e6dbab906bc
John Knoll <jpknoll@gmail.com>
2018-01-03 15:57:19 -0800

    add min lengths

--- Sloth.Core/Models/Scrubber.cs
+++ Sloth.Core/Models/Scrubber.cs
@@ -42,10 +42,20 @@ namespace Sloth.Core.Models
         [Required]
         public int BatchSequenceNumber { get; set; }
 
+        /// <summary>
+        /// Unique feed origination identifier given to the Feed System.
+        /// The origination code is validated in during file receipt and in the processing.
+        /// </summary>
+        [MinLength(2)]
         [MaxLength(2)]
         [Required]
         public string OriginCode { get; set; }
 
+        /// <summary>
+        /// Financial System document type associated with the feed.
+        /// Feed systems will be authorized to use a specific value based on transactions.
+        /// </summary>
+        [MinLength(4)]
         [MaxLength(4)]
         [Required]
         public string DocumentType { get; set; }
