c169de3c8ec43ad5901e4cfbf52f3ed2f64eb9f2
John Knoll <jpknoll@gmail.com>
2018-01-03 15:58:09 -0800

    add sources ref

--- Sloth.Sql/dbo/Tables/Sources.sql
+++ Sloth.Sql/dbo/Tables/Sources.sql
@@ -1,10 +1,12 @@
 CREATE TABLE [dbo].[Sources]
 (
-	[Id]           NVARCHAR(36)  NOT NULL PRIMARY KEY,
+	[Id]           NVARCHAR(36)  NOT NULL,
 	[Name]         NVARCHAR(50)  NOT NULL, 
     [Type]         NVARCHAR(50)  NOT NULL, 
-    [Description]  NVARCHAR(MAX) NOT NULL, 
+    [Description]  NVARCHAR(255) NULL, 
     [OriginCode]   NVARCHAR(2)   NOT NULL, 
     [DocumentType] NVARCHAR(4)   NOT NULL, 
+    [TeamId]       NVARCHAR(36)  NOT NULL, 
     CONSTRAINT [PK_Sources] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Sources_Teams_TeamId] FOREIGN KEY ([TeamId]) REFERENCES [dbo].[Teams] ([Id]),
 )
