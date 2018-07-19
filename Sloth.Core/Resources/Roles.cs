9b676ff242dfbf12274c443f79098f9937451141
John Knoll <jpknoll@gmail.com>
2018-07-19 13:55:59 -0700

    make roles const
    so we can use them in attributes

--- Sloth.Core/Resources/Roles.cs
+++ Sloth.Core/Resources/Roles.cs
@@ -1,17 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Sloth.Core.Resources
 {
     public class Roles
     {
         // System Roles
-        public static string SystemAdmin = "SystemAdmin";
+        public const string SystemAdmin = "SystemAdmin";
 
         // Team Roles
-        public static string Admin = "Admin";
-        public static string Approver = "Approver";
+        public const string Admin = "Admin";
+        public const string Approver = "Approver";
 
         internal static string[] GetAllRoles()
         {
