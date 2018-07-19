653816a9d0d6a8ed00b70fa0851d1585b864ae32
John Knoll <jpknoll@gmail.com>
2018-07-18 17:09:32 -0700

    Find and Create user methods

--- Sloth.Web/Controllers/UsersController.cs
+++ Sloth.Web/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sloth.Core;
 using Sloth.Core.Models;
+using Sloth.Core.Services;
 
 namespace Sloth.Web.Controllers
 {
@@ -13,11 +14,13 @@ namespace Sloth.Web.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SlothDbContext _context;
+        private readonly IDirectorySearchService _directorySearchService;
 
-        public UsersController(UserManager<User> userManager, SlothDbContext context)
+        public UsersController(UserManager<User> userManager, SlothDbContext context, IDirectorySearchService directorySearchService)
         {
             _userManager = userManager;
             _context = context;
+            _directorySearchService = directorySearchService;
         }
 
         public async Task<IActionResult> Manage()
@@ -27,6 +30,114 @@ namespace Sloth.Web.Controllers
                 .FirstOrDefaultAsync(u => u.Id == userId);
 
             return View(user);
+        }
+
+        public async Task<IActionResult> FindUser(string query)
+        {
+            // check database first
+            var dbUser = await _context.Users
+                .FirstOrDefaultAsync(u => u.UserName == query || u.Email == query);
+
+            if (dbUser != null)
+            {
+                return new JsonResult(new
+                {
+                    success = true,
+                    exists = true,
+                    user = new
+                    {
+                        id       = dbUser.Id,
+                        username = dbUser.UserName,
+                        email    = dbUser.Email,
+                        fullname = dbUser.FullName,
+                    },
+                });
+            }
+
+            // check directory
+            var directoryUser =
+                   await _directorySearchService.GetByKerberos(query)
+                ?? await _directorySearchService.GetByEmail(query);
+
+            if (directoryUser != null)
+            {
+                return new JsonResult(new
+                {
+                    success = true,
+                    exists = false,
+                    user = new
+                    {
+                        username = directoryUser.Kerberos,
+                        email = directoryUser.Email,
+                        fullname = directoryUser.FullName,
+                    },
+                });
+            }
+
+            return NotFound(new
+            {
+                success = false,
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUserFromDirectory(string query)
+        {
+            // check database first
+            var dbUser = await _context.Users
+                .FirstOrDefaultAsync(u => u.UserName == query || u.Email == query);
+
+            // user already exists
+            if (dbUser != null)
+            {
+                return new JsonResult(new
+                {
+                    success = true,
+                    exists = true,
+                    user = new
+                    {
+                        id       = dbUser.Id,
+                        username = dbUser.UserName,
+                        email    = dbUser.Email,
+                        fullname = dbUser.FullName,
+                    },
+                });
+            }
+
+            // find directory user
+            var directoryUser =
+                await _directorySearchService.GetByKerberos(query)
+                ?? await _directorySearchService.GetByEmail(query);
+
+            // can't find directory user
+            if (directoryUser == null)
+            {
+                return NotFound(new
+                {
+                    success = false,
+                });
+            }
+
+            // create user
+            var user = new User
+            {
+                UserName = directoryUser.Kerberos,
+                Email = directoryUser.Email,
+                FullName = directoryUser.FullName,
+            };
+            await _userManager.CreateAsync(user);
+
+            return new JsonResult(new
+            {
+                success = true,
+                user = new
+                {
+                    id       = user.Id,
+                    username = user.UserName,
+                    email    = user.Email,
+                    fullname = user.FullName,
+                }
+            });
         }
     }
 }
