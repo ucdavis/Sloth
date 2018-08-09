cf7618f3b7ab7147fe214b02b8e8bb2b3d804fab
John Knoll <jpknoll@gmail.com>
2018-08-09 13:41:50 -0700

    new properties
    scrubbers now have a source

--- Sloth.Core/Models/Team.cs
+++ Sloth.Core/Models/Team.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
@@ -17,6 +18,47 @@ namespace Sloth.Core.Models
         public string Id { get; set; }
 
         public string Name { get; set; }
+
+        /// <summary>
+        /// Campus User ID of main contact responsible for KFS feed.
+        /// </summary>
+        [MinLength(2)]
+        [MaxLength(8)]
+        [Required]
+        public string KfsContactUserId { get; set; }
+
+        /// <summary>
+        /// Email address used to send information when errors occur or other events with the KFS feed.
+        /// The email should match the origination code email.
+        /// A distribution list is highly recommended.
+        /// </summary>
+        [MaxLength(40)]
+        [EmailAddress]
+        [Required]
+        public string KfsContactEmail { get; set; }
+
+        /// <summary>
+        /// Contact number for personnel/unit responsible for KFS feed.
+        /// </summary>
+        [MinLength(1)]
+        [MaxLength(10)]
+        [RegularExpression("[0-9]*")]
+        [Required]
+        public string KfsContactPhoneNumber { get; set; }
+
+        /// <summary>
+        /// Brief mailing address for Department responsible for KFS feed.
+        /// </summary>
+        [MaxLength(30)]
+        [Required]
+        public string KfsContactMailingAddress { get; set; }
+
+        /// <summary>
+        /// Department responsible for KFS feed.
+        /// </summary>
+        [MaxLength(30)]
+        [Required]
+        public string KfsContactDepartmentName { get; set; }
 
         [JsonIgnore]
         public IList<ApiKey> ApiKeys { get; set; }
--- Sloth.Sql/dbo/Tables/Teams.sql
+++ Sloth.Sql/dbo/Tables/Teams.sql
@@ -1,8 +1,15 @@
 ﻿CREATE TABLE [dbo].[Teams] (
-    [Id]   NVARCHAR (450) NOT NULL,
-    [Name] NVARCHAR (450) NULL,
+    [Id]                       NVARCHAR (450) NOT NULL,
+    [KfsContactDepartmentName] NVARCHAR (30)  NOT NULL,
+    [KfsContactEmail]          NVARCHAR (40)  NOT NULL,
+    [KfsContactMailingAddress] NVARCHAR (30)  NOT NULL,
+    [KfsContactPhoneNumber]    NVARCHAR (10)  NOT NULL,
+    [KfsContactUserId]         NVARCHAR (8)   NOT NULL,
+    [Name]                     NVARCHAR (450) NULL,
     CONSTRAINT [PK_Teams] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
