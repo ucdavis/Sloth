1519c75683ccf0872d53fe0f37966c5993502fde
John Knoll <jpknoll@gmail.com>
2018-08-17 11:07:51 -0700

    expand tests

--- Sloth.Test/Xml/SerializationTests.cs
+++ Sloth.Test/Xml/SerializationTests.cs
@@ -21,7 +21,7 @@ namespace Sloth.Test.Xml
                 Header = new Header()
             };
 
-            target.Entries.Add(new EntryWithDetail());
+            target.Entries.Add(new Entry());
             target.Entries.Add(new EntryWithDetail(){ Detail = new Detail() });
             target.Entries.Add(new EntryWithDetail(){ Detail = new Detail() });
 
@@ -62,11 +62,47 @@ namespace Sloth.Test.Xml
             var xs = new XmlSerializer(typeof(Batch));
             var actual = (Batch)xs.Deserialize(sr);
 
-            // assert
+            // check structure
             Assert.NotNull(actual);
             Assert.NotNull(actual.Header);
-            Assert.Equal(actual.Entries.Count, 13);
+            Assert.Equal(13, actual.Entries.Count);
             Assert.NotNull(actual.Trailer);
+
+            // check header
+            var header = actual.Header;
+            Assert.Equal("3", header.Chart);
+            Assert.Equal("ACCT", header.OrganizationCode);
+            Assert.Equal(new DateTime(2017, 05, 08), header.BatchDate);
+            Assert.Equal(2, header.BatchSequenceNumber);
+            Assert.Equal("jackiev", header.ContactUserId);
+            Assert.Equal("javelasquez@ucdavis.edu", header.ContactEmail);
+            Assert.Equal(CampusCode.DV, header.CampusCode);
+            Assert.Equal("5307578571", header.ContactPhoneNumber);
+            Assert.Equal("1441 Research Park Drive", header.MailingAddress);
+            Assert.Equal("AdminIT", header.DepartmentName);
+
+            // check entries
+            var entry = actual.Entries[0];
+            Assert.Equal(2017, entry.FiscalYear);
+            Assert.Equal("3", entry.Chart);
+            Assert.Equal("6620001", entry.Account);
+            Assert.Equal("7259", entry.ObjectCode);
+            Assert.Equal(FinancialBalanceTypeCode.AC, entry.BalanceType);
+            Assert.Equal(UniversityFiscalPeriodCode.Period11, entry.FiscalPeriod);
+            Assert.Equal(FinancialDocumentTypeCode.GLJV, entry.DocType);
+            Assert.Equal("92", entry.OriginCode);
+            Assert.Equal("ADOCUMENT1", entry.DocumentNumber);
+            Assert.Equal(1, entry.SequenceNumber);
+            Assert.Equal("Some useful description", entry.Description);
+            Assert.Equal((decimal) 162.22, entry.Amount);
+            Assert.Equal(TransactionDebitCreditCode.Debit, entry.DebitCredit);
+            Assert.Equal(new DateTime(2017, 05, 01), entry.TransactionDate);
+            Assert.Equal("TESTTHIS1", entry.TrackingNumber);
+
+            // check trailer
+            var trailer = actual.Trailer;
+            Assert.Equal(13, trailer.TotalRecords);
+            Assert.Equal((decimal) 2031.20, trailer.TotalAmount);
         }
     }
 }
