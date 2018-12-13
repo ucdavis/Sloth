d5f141bfc06ebbb707a60eb8ada3d150cf898b30
John Knoll <jpknoll@gmail.com>
2018-12-13 15:56:02 -0800

    make common model

--- Sloth.Core/Models/CybersourceBankReconcileJobRecord.cs
+++ Sloth.Core/Models/CybersourceBankReconcileJobRecord.cs
@@ -1,25 +1,13 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace Sloth.Core.Models
 {
-    public class CybersourceBankReconcileJobRecord
+    public class CybersourceBankReconcileJobRecord : JobRecord
     {
-        public string Id { get; set; }
-
-        public string Name { get; set; }
-
-        [Display(Name = "Ran On")]
-        public DateTime RanOn { get; set; }
-
-        public string Status { get; set; }
-
         [Display(Name = "Processed Date")]
         public DateTime ProcessedDate { get; set; }
-
-        public IList<LogMessage> Logs { get; set; }
 
         public static void OnModelCreating(ModelBuilder modelBuilder)
         {
