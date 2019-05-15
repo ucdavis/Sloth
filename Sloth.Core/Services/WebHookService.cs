39c7e2d336c8cadb84bdeadbacf881373fe0358d
John Knoll <jpknoll@gmail.com>
2019-05-15 16:38:17 -0700

    add webhook service and payloads

---
+++ Sloth.Core/Services/WebHookService.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Serilog;
+using Sloth.Core.Models;
+using Sloth.Core.Models.WebHooks;
+
+namespace Sloth.Core.Services
+{
+    public interface IWebHookService
+    {
+        Task SendBankReconcileWebHook(Team team, BankReconcileWebHookPayload payload);
+
+        Task TestWebHook(WebHook webHook);
+    }
+
+    public class WebHookService : IWebHookService
+    {
+        private readonly SlothDbContext _dbContext;
+
+        public WebHookService(SlothDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task SendBankReconcileWebHook(Team team, BankReconcileWebHookPayload payload)
+        {
+            // fetch webhooks for this team and ship the payload
+            var hooks = await _dbContext.WebHooks.Where(w => w.Team.Id == team.Id).ToListAsync();
+
+            foreach (var hook in hooks)
+            {
+                await SendWebHookPayload(hook, payload);
+            }
+        }
+
+        public async Task TestWebHook(WebHook webHook)
+        {
+            var payload = new TestWebHookPayload()
+            {
+                HookId = webHook.Id,
+            };
+            await SendWebHookPayload(webHook, payload);
+        }
+
+        private async Task SendWebHookPayload(WebHook webHook, WebHookPayload payload)
+        {
+            using (var client = new HttpClient())
+            {
+                var data = JsonConvert.SerializeObject(payload);
+
+                var body = new StringContent(data, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync(webHook.Url, body);
+
+                // log response
+                Log.ForContext("webhook", webHook, true)
+                    .ForContext("response", response, true)
+                    .Information("Sent webhook");
+            }
+        }
+    }
+}
