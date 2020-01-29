edc4c7ad332300e4472bd8b0b8c12c01424dd43f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2020-01-29 10:10:16 -0800

    Catch in the loop

--- Sloth.Core/Jobs/CybersourceBankReconcileJob.cs
+++ Sloth.Core/Jobs/CybersourceBankReconcileJob.cs
@@ -40,7 +40,16 @@ namespace Sloth.Core.Jobs
 
                 foreach (var integration in integrations)
                 {
-                    await _cyberSourceBankReconcileService.ProcessIntegration(integration, date, log);
+                    try
+                    {
+                        await _cyberSourceBankReconcileService.ProcessIntegration(integration, date, log);
+                    }
+                    catch (Exception ex)
+                    {
+                        log.Error(ex, ex.Message);
+                        continue;
+                    }
+                    
                 }
             }
             catch (Exception ex)
