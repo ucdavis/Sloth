e92617a2bf2fc8e79a279da97ee15985fa46a472
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2020-09-23 09:40:16 -0700

    Only fetch WebHookRequests for retry older than N minutes

--- Sloth.Core/Configuration/WebHookServiceOptions.cs
+++ Sloth.Core/Configuration/WebHookServiceOptions.cs
@@ -5,5 +5,7 @@ namespace Sloth.Core.Configuration
         public int RetryDelaySeconds { get; set; } = 1;
 
         public int MaxRetryDelaySeconds { get; set; } = 32;
+
+        public int MinFetchAgeMinutes { get; set; } = 10;
     }
 }
