dd8ffc3f399e6a2c0f2e9e3f0706ffc46ee33101
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2021-07-12 15:59:07 -0700

    Restore some comments

--- Sloth.Core/Extensions/DateTimeExtensions.cs
+++ Sloth.Core/Extensions/DateTimeExtensions.cs
@@ -22,21 +22,21 @@ namespace Sloth.Core.Extensions
         }
 
         /// <summary>
-        /// Fiscal Year runs from July 1 to Jun 30
+        /// Returns the Fiscal Year according to the date provided to be used for transactions.
+        /// Fiscal Year runs from July 1 to Jun 30.
         /// </summary>
-        /// <param name="date"></param>
-        /// <returns></returns>
+        /// <returns>Fiscal Year</returns>
         public static int GetFinancialYear(this DateTime date)
         {
             return date.Month >= 7 ? date.Year + 1 : date.Year;
         }
 
         /// <summary>
+        /// Returns the Fiscal Period according to date provided to be used for transactions.
         ///  Fiscal Period is the month designator for the fiscal year.
         ///    So Jul = 1, Dec = 6, Jan = 7, Jun = 12
         /// </summary>
-        /// <param name="date"></param>
-        /// <returns></returns>
+        /// <returns>Fiscal Period</returns>
         public static int GetFiscalPeriod(this DateTime date)
         {
             return (date.Month + 5) % 12 + 1;
