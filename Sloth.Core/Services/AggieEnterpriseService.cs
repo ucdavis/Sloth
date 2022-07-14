780889a139b37f27176ac467a4b1e875b177ac9c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-07-14 08:55:32 -0700

    Put in the team name for now in the JournalDesc

--- Sloth.Core/Services/AggieEnterpriseService.cs
+++ Sloth.Core/Services/AggieEnterpriseService.cs
@@ -124,6 +124,7 @@ namespace Sloth.Core.Services
                     JournalSourceName = _journalSource,
                     JournalCategoryName = _journalCategory,
                     JournalName = "Sloth Recharges",
+                    JournalReference = source.Team.Name,
                     // TODO: should we add anything for journal name/desc/ref?  how does it work with batching?
                     AccountingDate = accountingDate?.ToString("yyyy-mm-dd"),
                     JournalLines = lines
