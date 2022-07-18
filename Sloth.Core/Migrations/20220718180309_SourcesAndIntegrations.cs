adb6c3d0776a6d9909199a7c209c113449bf35da
Scott Kirkland <srkirkland@gmail.com>
2022-07-18 11:05:02 -0700

    remove some required and update max string length for accts

---
+++ Sloth.Core/Migrations/20220718180309_SourcesAndIntegrations.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sloth.Core.Migrations
+{
+    public partial class SourcesAndIntegrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginCode",
+                table: "Sources",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2)",
+                oldMaxLength: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrganizationCode",
+                table: "Sources",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DocumentType",
+                table: "Sources",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Chart",
+                table: "Sources",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2)",
+                oldMaxLength: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HoldingAccount",
+                table: "Integrations",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClearingAccount",
+                table: "Integrations",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginCode",
+                table: "Sources",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2)",
+                oldMaxLength: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "OrganizationCode",
+                table: "Sources",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "DocumentType",
+                table: "Sources",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldMaxLength: 4,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Chart",
+                table: "Sources",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2)",
+                oldMaxLength: 2,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "HoldingAccount",
+                table: "Integrations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClearingAccount",
+                table: "Integrations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+        }
+    }
+}
