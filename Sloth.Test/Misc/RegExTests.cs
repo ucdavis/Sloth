f54d4f03f002b5410c48f87462d7523942c63dfd
Jason Sylvestre <jsylvestre@ucdavis.edu>
2022-07-20 12:55:43 -0700

    Other regex with one failing

--- Sloth.Test/Misc/RegExTests.cs
+++ Sloth.Test/Misc/RegExTests.cs
@@ -20,10 +20,54 @@ namespace Sloth.Test.Misc
         [InlineData("qrstuvwkyz", "qrstuvwkyz")]
         [InlineData("QRSTUVWKYZ", "QRSTUVWKYZ")]
         [InlineData("__--'", "__--")]
+        [InlineData("!@#$%^&*()+=~;:<>,.?[]{}|Meh'", "Meh")]
         public void TestStripToGlReferenceField25(string passed, string expected)
         {
             var rtValue = passed.StripToGlReferenceField(25);
             rtValue.ShouldBe(expected);
         }
+
+
+        /// <summary>
+        /// @"[^0-9a-zA-Z\ \.\-_]+"
+        /// </summary>
+        /// <param name="passed"></param>
+        /// <param name="expected"></param>
+        [Theory]
+        [InlineData("Aggie Enterprise", "Aggie Enterprise")]
+        [InlineData("Aggie*&^%&^%&^$&^%$Enterprise", "AggieEnterprise")]
+        [InlineData("123456789012345678901234567890", "1234567890123456789012345")]
+        [InlineData("abcdefghijklmnop", "abcdefghijklmnop")]
+        [InlineData("ABCDEFGHIJKLMNOP", "ABCDEFGHIJKLMNOP")]
+        [InlineData("qrstuvwkyz", "qrstuvwkyz")]
+        [InlineData("QRSTUVWKYZ", "QRSTUVWKYZ")]
+        [InlineData("__--.'", "__--.")]
+        [InlineData("!@#$%^&*()+=~;:<>,.?[]{}|Meh'", ".Meh")]        
+        public void TestStripToErpDescription25(string passed, string expected)
+        {
+            var rtValue = passed.StripToErpDescription(25);
+            rtValue.ShouldBe(expected);
+        }
+
+        /// <summary>
+        /// @"[^0-9a-zA-Z\ \-_]+"
+        /// </summary>
+        /// <param name="passed"></param>
+        /// <param name="expected"></param>
+        [Theory]
+        [InlineData("Aggie Enterprise", "Aggie Enterprise")]
+        [InlineData("Aggie*&^%&^%&^$&^%$Enterprise", "AggieEnterprise")]
+        [InlineData("123456789012345678901234567890", "1234567890123456789012345")]
+        [InlineData("abcdefghijklmnop", "abcdefghijklmnop")]
+        [InlineData("ABCDEFGHIJKLMNOP", "ABCDEFGHIJKLMNOP")]
+        [InlineData("qrstuvwkyz", "qrstuvwkyz")]
+        [InlineData("QRSTUVWKYZ", "QRSTUVWKYZ")]
+        [InlineData("__--.'", "__--.")]
+        [InlineData("!@#$%^&*()+=~;:<>,.?[]{}|Meh'", "Meh")]
+        public void TestStripToErpName25(string passed, string expected)
+        {
+            var rtValue = passed.StripToErpName(25);
+            rtValue.ShouldBe(expected);
+        }        
     }
 }
