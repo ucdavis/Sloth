63e668c6db84630370b0b702347df43a09176400
Scott Kirkland <srkirkland@gmail.com>
2022-08-01 10:42:14 -0700

    make a new class for storing job runs

---
+++ Sloth.Core/Domain/JobRecord.cs
@@ -0,0 +1,44 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Sloth.Core.Models
+{
+    public class JobRecord
+    {
+        public JobRecord()
+        {
+            StartedAt = DateTime.UtcNow;
+        }
+
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
+
+        public DateTime StartedAt { get; set; }
+
+        public DateTime? EndedAt { get; set; }
+
+        [StringLength(32)]
+        public string Status { get; set; }
+
+        public string Details { get; set; }
+
+        public void SetDetails(object details)
+        {
+            // convert to json string and store
+            Details = JsonConvert.SerializeObject(details);
+        }
+
+        // list of possible statuses
+        public static class Statuses
+        {
+            public const string Pending = "Pending";
+            public const string Running = "Running";
+            public const string Success = "Success";
+            public const string Failed = "Failed";
+        }
+    }
+}
