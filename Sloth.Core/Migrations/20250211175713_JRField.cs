56190b86a5f62a34e69c67d90fc25ace62f5fef4
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-02-11 09:57:58 -0800

    migration

---
+++ Sloth.Core/Migrations/20250211175713_JRField.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sloth.Core.Migrations
+{
+    public partial class JRField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SavedTransactionId",
+                table: "JournalRequests",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SavedTransactionId",
+                table: "JournalRequests");
+        }
+    }
+}
